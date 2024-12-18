89a0e782a213d684ec5841518b7c80b9036bef17
tutu140997 <camtule419@gmail.com>
2024-12-19 00:02:41 +0700

    update 1

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="mx-auto px-4 py-0 border border-gray-200 !bg-gray-100 ">
       <h2 className="text-xl font-semibold mt-2 mb-2 text-center text-black">
-        Kokoro JapaneseDining
+        Kokoro Japanese Dining
       </h2>
     </div>
   );
